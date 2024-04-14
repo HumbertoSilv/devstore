06ac0f505362ff25d50620f2abbfcb4dba330b96
Humbert0 <matheushumberto520@gmail.com>
2024-04-14 19:44:48 -0400

    feat: updates to CartProvider

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,10 +1,22 @@
+import { Metadata } from 'next'
 import Image from 'next/image'
+import { AddToCartButton } from '../../../../components/add-to-cart-button'
 import { api } from '../../../../data/api'
 import { Product } from '../../../../data/types/product'
 
 interface ProductProps {
   params: {
     slug: string
+  }
+}
+
+export async function generateMetadata({
+  params,
+}: ProductProps): Promise<Metadata> {
+  const product = await getProduct(params.slug)
+
+  return {
+    title: product.title,
   }
 }
 
@@ -19,6 +31,16 @@ async function getProduct(slug: string): Promise<Product> {
 
   return product
 }
+
+export async function generateStaticParams() {
+  const response = await api('/products/featured')
+  const products: Product[] = await response.json()
+
+  return products.map((product) => {
+    return { slug: product.slug }
+  })
+}
+
 export default async function ProductPage({ params }: ProductProps) {
   const product = await getProduct(params.slug)
 
@@ -43,15 +65,18 @@ export default async function ProductPage({ params }: ProductProps) {
 
         <div className="mt-8 flex items-center gap-3">
           <span className="inline-block rounded-full bg-violet-500 px-5 py-2.5 font-semibold">
-            R$129
-          </span>
-          <span className="text-sm text-zinc-400">
-            Em 12x s/ juros de{' '}
             {product.price.toLocaleString('pt-BR', {
               style: 'currency',
               currency: 'BRL',
               minimumFractionDigits: 0,
               maximumFractionDigits: 0,
+            })}
+          </span>
+          <span className="text-sm text-zinc-400">
+            Em 12x s/ juros de{' '}
+            {(product.price / 12).toLocaleString('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
             })}
           </span>
         </div>
@@ -87,12 +112,7 @@ export default async function ProductPage({ params }: ProductProps) {
           </div>
         </div>
 
-        <button
-          type="button"
-          className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold text-white"
-        >
-          Adicionar ao carrinho
-        </button>
+        <AddToCartButton productId={product.id} />
       </div>
     </div>
   )
